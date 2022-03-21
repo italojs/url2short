6f3c644a04ef01567c540d8b73570c2f5061f4f5
vortx <italo.i@live.com>
2022-03-21 20:32:29 -0300

    rename project

--- src/infra/config/mongo.js
+++ src/infra/config/mongo.js
@@ -3,6 +3,6 @@ require('dotenv').config()
 
 
 module.exports = {
-  dbName: env.get(`$MONGO_DATABASE`, 'testBlueCoding'),
+  dbName: env.get(`$MONGO_DATABASE`, 'testurl2short'),
   connstr: env.get(`$MONGO_CONN_STR`, 'mongodb://localhost:27017'),
 }
